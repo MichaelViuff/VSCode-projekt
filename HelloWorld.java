4b8135d93671e0d33f09ddb4da8acf40afb1d26f
MichaelViuff <mivi@via.dk>
2025-09-19 09:47:27 +0200

    Fra IntelliJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from VSCode!");
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from IntelliJ!");
     }
 }
